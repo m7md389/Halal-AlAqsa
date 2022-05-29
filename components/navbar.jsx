056937ab1071800a1679fa789da12dda6c6cd776
Mohammad <m7md389@gmail.com>
2022-05-30 00:08:19 +0300

    sleep

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,12 +1,30 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import navbarStyles from "../styles/Navbar.module.scss";
 
 export default function Home() {
   return (
     <nav className={navbarStyles.navbar}>
       <ul className={navbarStyles.navLinks}>
-        <li className={navbarStyles.navLink}>Home</li>
-        <li className={navbarStyles.navLink}>About</li>
+        <li onClick={() => {}} className={getNavlinkClass("/")}>
+          <Link href="/">Home</Link>
+        </li>
+        <li onClick={() => {}} className={getNavlinkClass("/about")}>
+          <Link href="/about">About</Link>
+        </li>
       </ul>
     </nav>
   );
 }
+
+const onClickNavLink = () => {};
+
+const getNavlinkClass = (link) => {
+  const currentPath = useRouter().asPath;
+
+  if (currentPath === link) {
+    return `${navbarStyles.navLink} ${navbarStyles.navLinkActive}`;
+  }
+
+  return navbarStyles.navLink;
+};
---
+++ pages/about.jsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Image from "next/image";
+import Hero from "./../components/Hero";
+import Card from "./../components/Card";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Hero
+          title={"About us"}
+          paragraph={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer purus."
+          }
+          img={{
+            url: "https://www.canon-printersetupsupport.com/images/about-us.jpg",
+            desc: "Test image."
+          }}
+        />
+
+        <Card
+          title={"About us"}
+          description={
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          }
+        />
+      </main>
+    </div>
+  );
+}
